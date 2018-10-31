7b5298b1205e75dc425fb399b4b406084601b3a1
TazhibekA <40587394+TazhibekA@users.noreply.github.com>
2018-10-31 21:29:52 +0600

    Add files via upload

---
+++ ConsoleApp12/ConsoleApp12/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> collection = new List<int>();
+            collection.Add(1);
+            collection.Add(2);
+            collection.Add(3);
+            collection.Add(4);
+            collection.Add(5);
+            int sumElement = 0;
+            
+
+            for (int i = 0; i < collection.Count; i++)
+            {
+                if (i % 2 == 0)
+                    sumElement = sumElement + collection[i];
+               
+
+            }
+ 
+            int maxPosition = 0;
+            int resultPosition = 0;
+            int resultElement = 0;
+
+            for (int i = 0; i < collection.Count; i++)
+            {
+                if (collection[i] == collection.Max()){
+                    maxPosition = i;
+                    collection.Remove(collection[i]);
+                }
+            }
+ 
+            for (int i = 0; i < collection.Count; i++)
+            {
+                if (collection[i] == collection.Max()){
+                    resultPosition = i;
+                    resultElement = collection[i];
+                }
+            }
+ 
+            if (maxPosition > resultPosition)
+            {
+                Console.WriteLine("Second max element : " + resultElement + " Position: " + resultPosition );
+            }
+            else
+            {
+                Console.WriteLine("Second max element : " + resultElement + " Position: " + (resultPosition + 1) );
+            }
+
+            Console.WriteLine("Sum: " + sumElement );
+
+            Console.ReadLine();
+        }
+    }
+}
